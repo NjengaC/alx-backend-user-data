c2c332adafab0031ed0f60f7fa7e0ceaa287d927
NjengaC <victorcyrus01@gmail.com>
2024-07-03 21:06:05 +0300

    refactor

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,19 +37,13 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
-    """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    """Get the database of the application"""
     connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
+            user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+            host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+            password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+            database=os.environ.get("PERSONAL_DATA_DB_NAME", "my_db")
+            )
     return connection
 
 
