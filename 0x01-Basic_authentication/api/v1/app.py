aece16065458b647725002dc9b3908b0222ea0fa
NjengaC <victorcyrus01@gmail.com>
2024-07-08 23:45:50 +0300

    reverse

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,10 +18,8 @@ auth = None
 auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
+if auth_type == "basic_auth":
     auth = BasicAuth()
 
 
