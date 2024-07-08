c47e8bef3957771da7b87c30d10ff6e54b847c42
NjengaC <victorcyrus01@gmail.com>
2024-07-08 18:14:55 +0300

    strict slashes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,15 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
-def get_unauthorized() -> str:
-    """
-    unauthorized test
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
     """
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'])
-def get_forbidden() -> str:
-    """ forbidden test """
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
     abort(403)
