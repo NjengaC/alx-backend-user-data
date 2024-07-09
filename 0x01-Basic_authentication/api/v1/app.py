fe64319d635b565a1720b7931af522c8003e42ca
NjengaC <victorcyrus01@gmail.com>
2024-07-09 13:43:06 +0300

    forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,6 +30,12 @@ def unauthorized(error) -> str:
                    }), 401
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden request handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
