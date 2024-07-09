f1a1987b39737dc6a8c7cc18ef96707bcfc4637b
NjengaC <victorcyrus01@gmail.com>
2024-07-09 13:57:10 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,38 @@
+#!/usr/bin/env python3
+"""Authentication module for the API.
+"""
+import re
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
         """
-        This method will be used to check if a path requires authentication.
-        Currently, it returns False as a placeholder.
-        """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request.
         """
-        This method will be used to get the Authorization
-        header from the request.
-        Currently, it returns None as a placeholder.
-        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        This method will be used to get the current user from the request.
-        Currently, it returns None as a placeholder.
+        """Gets the current user from the request.
         """
         return None
